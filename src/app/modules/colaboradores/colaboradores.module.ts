a34c815b62bc2137b7412c19aa1df41971d07665
ddomingos.ext <ddomingos.ext@orizon.com.br>
2023-09-16 00:02:51 -0300

    colaborador

--- src/app/modules/colaboradores/colaboradores.module.ts
+++ src/app/modules/colaboradores/colaboradores.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { ColaboradoresRoutingModule } from './colaboradores.routing';
 import { ColaboradoresPage } from './pages/colaboradores.page';
 import { ComponentsModule } from '@app/shared/components/components/components.module';
+import { MaterialModule } from '@app/shared/components/material.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { ComponentsModule } from '@app/shared/components/components/components.m
   imports: [
     ColaboradoresRoutingModule, 
     ComponentsModule,
+    MaterialModule,
   ],
   providers: [],
 })
