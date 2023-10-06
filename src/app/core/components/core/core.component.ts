4a330e218f54abe40e8503f32fa97761286ee5ca
ddomingos.ext <ddomingos.ext@orizon.com.br>
2023-10-06 17:22:02 -0300

    O pai é brabo

--- src/app/core/components/core/core.component.ts
+++ src/app/core/components/core/core.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, ViewChild } from '@angular/core';
+import { MatDrawer, MatSidenav } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
 import { AuthService } from '@app/core/authentication/auth.service';
 
@@ -7,9 +9,30 @@ import { AuthService } from '@app/core/authentication/auth.service';
   styleUrls: ['./core.component.scss'],
 })
 export class CoreComponent {
+
+  @ViewChild(MatDrawer)
+  drawer!: MatDrawer;
+
   opened = true;
   
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(
+    private authService: AuthService, 
+    private router: Router,
+    private observer: BreakpointObserver) { 
+ 
+  }
+
+  ngAfterViewInit(){
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if(res.matches){
+        this.drawer.mode = 'over';
+        this.drawer.close();
+      }else{
+        this.drawer.mode = 'side'
+        this.drawer.open();
+      }
+    });
+  }
 
   toggle() {
     this.opened = !this.opened;
